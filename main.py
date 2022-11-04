58648821fb95b6b5b4d4b057b8696617e71caf5a
kolpakovpasa <102384642+kolpakovpasa@users.noreply.github.com>
2022-11-04 23:49:56 +0600

    завершен первый этап

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import datetime
 import requests
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-
+from sql import insert_user_into_db, insert_match_into_db, check_db_link, create_db_link
 
 with open('bot_token.txt', 'r') as file:
     bot_token = file.readline()
 with open('app_token.txt', 'r') as file:
     app_token = file.readline()
-with open('photo_token.txt', 'r') as file:
-    photo_token = file.readline()
+
 
 vk_session = vk_api.VkApi(token=bot_token)
 longpoll = VkLongPoll(vk_session)
 
 
-def write_msg(user_id, message, attachment=0):
+def write_msg(user_id, message, attachment='0'):
     vk_session.method('messages.send', {'user_id': user_id,
                                         'message': message,
                                         'random_id': randrange(10 ** 7),
@@ -25,13 +24,14 @@ def write_msg(user_id, message, attachment=0):
 
 
 def get_user_info(user_id):
+    """Takes user_id as an integer, returns user_info as a dictionary"""
     user_info = {}
     url = 'https://api.vk.com/method/users.get'
     params = {
         'user_ids': user_id,
         'access_token': app_token,
         'v': 5.131,
-        'fields': 'first_name, last_name, bdate, sex, city, relation, movies, music'
+        'fields': 'first_name, last_name, bdate, sex, city, movies, music'
     }
     if requests.get(url, params=params).json().get('response'):
         for key, value in requests.get(url, params=params).json().get('response')[0].items():
@@ -41,13 +41,12 @@ def get_user_info(user_id):
                 user_info[key] = value
     else:
         return False
-    # print(user_info)
     return user_info
 
 
-def check_user_info_missing(user_id):
+def check_user_info_missing(user_info):
+    """Takes user_info as a dictionary, returns keys missing in user_info as a list"""
     info_missing = []
-    user_info = get_user_info(user_id)
     for item in ['bdate', 'sex', 'city', 'movies', 'music']:
         if not user_info.get(item):
             info_missing.append(item)
@@ -56,85 +55,89 @@ def check_user_info_missing(user_id):
     return info_missing
 
 
-def translate_fields(fields: list):
+def translate_field(field):
+    """Takes a string in English, returns it in Russian also as a string"""
     dictionary = {
-        'bdate': 'Возраст в формате xx.xx.xxxx',
-        'sex': 'Пол (1 - женский, 2 - мужской)',
-        'city': 'Город',
-        'movies': 'Любимый фильм',
-        'music': 'Любимая музыка'
+        'bdate': 'ваша дата рождения в формате xx.xx.xxxx',
+        'sex': 'ваш пол (1 - женский, 2 - мужской)',
+        'city': 'ваш город',
+        'movies': 'ваш любимый фильм',
+        'music': 'ваша любимая музыка'
     }
-    translation = []
-    for field in fields:
-        translation.append(dictionary[field])
+    translation = dictionary[field]
     return translation
 
 
 def get_age(date):
+    """Takes a date of birth as a string in a xx.xx.xxxx format,
+    returns corresponding age as of current day as an integer"""
     return datetime.datetime.now().year - int(date[-4:])
 
 
-def get_additional_information(user_id):
-    info_missing = check_user_info_missing(user_id)
-    missing_fields_translated = translate_fields(info_missing)
-    fields_to_require = ''
-    for field in missing_fields_translated:
-        fields_to_require += f'{field}. '
-    write_msg(user_id, f'''Похоже, мы мало о вас знаем.
-    Пожалуйста, сообщите через запятую следующие данные:
-    {fields_to_require}''')
+def get_additional_information(user_id, field):
+    """Takes user_id as a dictionary to send a message and a field to require as a string,
+    returns an answer as a string """
+    write_msg(user_id, f'''Пожалуйста, сообщите следующие данные о себе:\n{translate_field(field)}''')
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
-                return [text.strip() for text in event.text.split(',')]
+                return event.text
 
 
-def find_matches(user_info: dict):
+def find_matches(user_info):
+    """Takes user_info as a dictionary, returns other users infos as a list of dictionaries"""
     url = 'https://api.vk.com/method/users.search'
     params = {
-        'age_from': user_info['age'] - 5,
-        'age_to': user_info['age'] + 5,
+        'age_from': user_info['age'] - 3,
+        'age_to': user_info['age'] + 3,
         'sex': 3 - user_info['sex'],
         'city': user_info['city'],
         'status': 6,
         'has_photo': 1,
+        'count': 1000,
         'access_token': app_token,
         'v': 5.131
-        }
+    }
     if requests.get(url, params=params).json().get('response'):
-        print(requests.get(url, params=params).json().get('response'))
         return requests.get(url, params=params).json().get('response').get('items')
 
 
-
-def choose_match(matches: list):
+def choose_match(matches, user_id):
+    """Takes users infos as a list of dictionaries and user_id as an integer,
+    makes some additional checks and filters for suitability,
+    returns a random but suitable user_info as a dictionary"""
     filter_success = False
     random_choice = {}
     while not filter_success:
         random_choice = random.choice(matches)
-        if get_photos(random_choice['id']) and not random_choice['is_closed']:
+        if get_photos(random_choice['id'])\
+                and not random_choice['is_closed']\
+                and random_choice['id'] != user_id\
+                and check_db_link(random_choice, user_id):
             filter_success = True
-            print('success')
     return random_choice
 
 
-
-
 def get_photos(user_id):
-    url = 'https://api.vk.com/method/photos.getAll'
+    """Takes user_id as an integer, returns 3 photos data as a dictionary"""
+    url = 'https://api.vk.com/method/photos.get'
     params = {
         'owner_id': user_id,
+        'album_id': 'profile',
         'extended': '1',
-        'access_token': photo_token,
+        'access_token': app_token,
         'v': 5.131
     }
     response = requests.get(url, params=params).json().get('response')
     if response:
         if response.get('count') < 3:
             return False
-        top_photos = sorted(response.get('items'), key=lambda x: x['likes']['count'], reverse=True)[:3]
-        return True
-
+        top_photos = sorted(response.get('items'), key=lambda x: x['likes']['count']
+                            + x['comments']['count'], reverse=True)[:3]
+        photo_data = {'user_id': top_photos[0]['owner_id'], 'photo_ids': []}
+        for photo in top_photos:
+            photo_data['photo_ids'].append(photo['id'])
+        return photo_data
 
 
 def main():
@@ -143,19 +146,29 @@ def main():
             if event.to_me:
                 user_info = get_user_info(event.user_id)
                 write_msg(event.user_id, f'''Привет, {user_info['first_name']}!
-                Сейчас подберём вам пару через VKinder.''')
-                info_missing = check_user_info_missing(event.user_id)
-                if info_missing:
-                    info_provided = dict(zip(info_missing, get_additional_information(event.user_id)))
-                    user_info.update(info_provided)
+Подберём вам пару через VKinder?
+Сейчас проверим, достаточно ли мы о вас знаем.''')
+                info_missing = check_user_info_missing(user_info)
+                while info_missing:
+                    additional_info = get_additional_information(event.user_id, info_missing[0])
+                    user_info[info_missing[0]] = additional_info
+                    info_missing.pop(0)
                 user_info['age'] = get_age(user_info['bdate'])
-                print(user_info)
+                insert_user_into_db(user_info)
                 write_msg(event.user_id, '''Информации о вас достаточно :)
-                Начинаем подбирать пары......''')
+Начинаем подбирать пары...''')
                 matches_found = find_matches(user_info)
-                match_found = choose_match(matches_found)
-                write_msg(event.user_id, f'''Мы нашли возможную пару: {match_found['first_name']} {match_found['last_name']}
-Вы можете найти этого пользователя по ссылке: vk.com/id{match_found['id']}''')
+                match_found = choose_match(matches_found, event.user_id)
+                photo_data = get_photos(match_found['id'])
+                insert_match_into_db(match_found, photo_data)
+                create_db_link(match_found, event.user_id)
+                write_msg(event.user_id,
+                          f'''Мы нашли возможную пару: {match_found['first_name']} {match_found['last_name']}
+Вы можете найти этого пользователя по ссылке: vk.com/id{match_found['id']}''',
+                          attachment=f"photo{photo_data['user_id']}_{photo_data['photo_ids'][0]},"
+                                     f"photo{photo_data['user_id']}_{photo_data['photo_ids'][1]},"
+                                     f"photo{photo_data['user_id']}_{photo_data['photo_ids'][2]}")
 
 
-main()
+if __name__ == '__main__':
+    main()
---
+++ sql.py
@@ -0,0 +1,54 @@
+import sqlalchemy
+import psycopg2
+
+
+DSN = 'postgresql://postgres:Gliese-581@localhost:5432/Vkinder'
+engine = sqlalchemy.create_engine(DSN)
+connection = engine.connect()
+
+
+def insert_user_into_db(user):
+    """User as a dictionary"""
+    if not connection.execute(f"SELECT id FROM users WHERE id = {user['id']};").fetchone():
+        connection.execute(f"INSERT INTO users (id, first_name, last_name, sex, bdate, city)"
+                           f"VALUES ({user['id']}, \'{user['first_name']}\',"
+                           f"\'{user['last_name']}\', {user['sex']},"
+                           f"\'{'-'.join(user['bdate'].split('.')[::-1])}\', {user['city']});")
+        print('User successfully added to db')
+    else:
+        print(f"User with id {user['id']} already in db")
+
+
+def insert_match_into_db(user, photo_data):
+    """User as a dictionary, photo_data as a dictionary"""
+    if not connection.execute(f"SELECT id FROM matches WHERE id = {user['id']};").fetchone():
+        connection.execute(f"INSERT INTO matches (id, first_name, last_name, top_photo_1, top_photo_2, top_photo_3)"
+                           f"VALUES ({user['id']}, \'{user['first_name']}\',"
+                           f"\'{user['last_name']}\', {photo_data['photo_ids'][0]},"
+                           f"{photo_data['photo_ids'][1]}, {photo_data['photo_ids'][2]});")
+        print('Match successfully added to db')
+    else:
+        print(f"Match with id {user['id']} already in db")
+
+
+def check_db_link(match, user_id):
+    """Match as a dictionary, user_id as an integer"""
+    if connection.execute(f'''SELECT user_id
+    FROM matches_to_users
+    WHERE user_id = {user_id} and match_id = {match['id']};''').fetchone():
+        print('Link already in db')
+        return False
+    print('No link in db yet')
+    return True
+
+
+def create_db_link(match, user_id):
+    """Match as a dictionary, user_id as an integer"""
+    if connection.execute(f'''SELECT user_id
+    FROM matches_to_users
+    WHERE user_id = {user_id} and match_id = {match['id']};''').fetchone():
+        print('Link already in db')
+        return False
+    connection.execute(f"INSERT INTO matches_to_users (user_id, match_id) VALUES ({user_id}, {match['id']});")
+    print('Link successfully added to db')
+    return True
